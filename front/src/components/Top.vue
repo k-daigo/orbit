cad7a3a5588fc5efee8f3b09b888376797825b7b
rsp01 <rsp01@example.com>
2019-08-31 22:18:35 +0900

    refactor

--- front/src/components/Top.vue
+++ front/src/components/Top.vue
@@ -4,8 +4,11 @@
       <img src="/images/logo.png">
     </div>
 
+    <!-- サイドバー -->
     <div class="menu" v-on:mouseover="onMenuMouseover" v-on:mouseleave="onMuneMouseleave">
       <div class="menuItem" v-show="meneItem">
+
+        <!-- 衛星名 -->
         <div class="dropdown">
           <a href="javascript:void(0)" class="white" v-on:click="onSelSatClick">{{target}}</a>
           <div id="selSatArea" class="dropdown-content">
@@ -20,19 +23,21 @@
         </div>
         <br>
 
-        <hr class="menuSep">
-        <a href="javascript:void(0)" class="white" v-on:click="onRealtimeClick">リアルタイム</a>
+        <!-- <hr class="menuSep">
+        <a href="javascript:void(0)" class="white" v-on:click="onRealtimeClick">リアルタイム</a> -->
         <hr class="menuSep">
         <a href="javascript:void(0)" class="white" v-on:click="loadTle">TLE再取得</a>
       </div>
     </div>
 
+    <!-- 表示中の衛星名と日時 -->
     <div class="sat-info">
       {{target}}
       &nbsp;&nbsp;&nbsp;&nbsp;
       {{clock}}
     </div>
 
+    <!-- 地図 -->
     <div id="map" class="map"></div>
   </div>
 </template>
@@ -71,6 +76,33 @@ export default {
       orbitLine: null,
       bounds: {},
       execProc: null,
+      
+      // 地図の拡大値による、時刻マーカーの表示間隔の設定
+      markerConfig: {
+        0: 1000,
+        1: 1000,
+        2: 500,
+        3: 300,
+        4: 200,
+        5: 100,
+        6: 50,
+        7: 30,
+        8: 30,
+        9: 5,
+        10: 5,
+        11: 2,
+        12: 2,
+        13: 1,
+        14: 1,
+        15: 1,
+        16: 1,
+        17: 1,
+        18: 1,
+        19: 1,
+        20: 1,
+        21: 1,
+        23: 1,
+      },
     }
   },
 
@@ -94,18 +126,19 @@ export default {
     // 時刻のリアルタイム更新
     runRealtimeClock() {
       this.dtIntervalId = setInterval(() => {
-        this.refreshClcok();
+        this.refresh();
       }, 1000)
     },
 
-    // 時刻更新
-    refreshClcok() {
+    // 定期更新
+    refresh() {
+      // 時刻更新
       this.clock = moment(new Date).format('YYYY/MM/DD HH:mm:ss');
-
       // 衛星の移動
       this.moveSatMarker();
-      },
+    },
 
+    // サイドメニューにマウスが乗ったらメニューを開く
     onMenuMouseover() {
       anime({
         targets: ['.menu'],
@@ -119,6 +152,7 @@ export default {
       this.meneItem = true;
     },
 
+    // サイドメニューからマウスが離れたらメニューを閉じる
     onMuneMouseleave() {
       anime({
         targets: ['.menu'],
@@ -132,10 +166,6 @@ export default {
       this.meneItem = false;
     },
 
-    onTleClick() {
-      this.loadTle();
-    },
-
     // TLE取得
     async loadTle() {
       this.orgTle = await this.getTleFromNorad();
@@ -143,6 +173,7 @@ export default {
       console.info(`TLE 取得完了`);
     },
 
+    // NORADからTLEを取得する
     async getTleFromNorad() {
       const that = this;
       return new Promise(resolve => {
@@ -155,20 +186,18 @@ export default {
       });
     },
 
-    onGetLonLatClick() {
-      this.viewSat();
-    },
-
     // 衛星の軌道をGoogleMapに描画
     viewSat() {
-      var myTLE = new orbits.TLE(this.orbitMap[this.target].tle);
-      var myOrbit = new orbits.Orbit(myTLE);
-      var mapArea = new google.maps.Map(document.getElementById("map"));
-      var mySat = new orbits.Satellite({ map: mapArea, tle: myTLE});
+      const currentTle = new orbits.TLE(this.orbitMap[this.target].tle);
+      // const myOrbit = new orbits.Orbit(currentTle);
+      const mapArea = new google.maps.Map(document.getElementById("map"));
+      this.satellite = new orbits.Satellite({ map: mapArea, tle: currentTle});
+
+      // 地図の初期設定
       var options = {
-        center: mySat.position, // 地図の場所
-        mapTypeId: google.maps.MapTypeId.ROADMAP,               // 地図の種類
-        zoom: 1,                                                // 地図の縮尺
+        center: this.satellite.position,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        zoom: 1,
         icon: null,
         streetViewControl: false,
         fullscreenControl: false,
@@ -178,17 +207,20 @@ export default {
       // 地図のズーム変更イベント
       const that = this;
       google.maps.event.addListener(this.gmap, 'zoom_changed', function() {
-        that.onGmapZoomChanged(this.gmap);
+        that.onGmapZoomChanged();
       });
       // 表示中の緯度経度の変更イベント
       google.maps.event.addListener(this.gmap, 'bounds_changed', function() {
         that.saveBounds();
       });
 
-      this.satellite = new orbits.Satellite({ map: mapArea, tle: myTLE});
+      // 軌道の描画
+      this.refreshOrbit();
+    },
 
-      // 軌道の再描画
-      this.refreshOrbitTimer();
+    // 地図の拡大縮小
+    onGmapZoomChanged() {
+      this.refreshOrbit();
     },
 
     // 衛星の現在位置を表示
@@ -208,6 +240,7 @@ export default {
       this.satMarker = new google.maps.Marker(markerOptions);
     },
 
+    // 衛星を現在位置に移動
     moveSatMarker() {
       let dt = new Date();
       this.satellite.setDate(dt);
@@ -215,17 +248,20 @@ export default {
       this.satMarker.setPosition(this.satellite.position);
     },
 
+    // 軌道の線と時刻マーカーをクリア
     clearMaker() {
       // 時刻マーカー
       this.gmapMakers.forEach(function (marker, idx) {
         marker.setMap(null);
       });
+
       // 軌道の線
       if(this.orbitLine != null){
         this.orbitLine.setMap(null);
       }
     },
 
+    // 地図の表示範囲を保持
     saveBounds() {
       const bounds = this.gmap.getBounds();
       this.bounds = {
@@ -234,10 +270,11 @@ export default {
         mapSwLat: bounds.getSouthWest().lat(),
         mapSwLng: bounds.getSouthWest().lng(),
       }
-      // console.debug(`saveBounds=${JSON.stringify(this.bounds)}`)
-      this.refreshOrbitTimer();
+
+      this.refreshOrbit();
     },
 
+    // 指定の緯度経度が地図の表示範囲内か判定する
     isSatInRange(lat, lng) {
       if(this.gmap == null) {
         return true;
@@ -257,45 +294,23 @@ export default {
       return true;
     },
 
-    refreshOrbitTimer() {
+    // 軌道などを更新する
+    refreshOrbit() {
+
+      // 500ms後に更新する。更新前に次の更新要求が来た場合は、前回の更新予約をキャンセルする
       if(this.execProc != null) {
         clearTimeout(this.execProc);
       }
-      this.execProc = setTimeout(this.refreshOrbit, 500);
+      this.execProc = setTimeout(this.viewOrbit, 500);
     },
 
-    refreshOrbit() {
+    // 軌道などを再描画する
+    viewOrbit() {
       this.execProc = null;
       console.debug(`getZoom=${this.gmap.getZoom()}`)
 
       // クリアしてから描画
       this.clearMaker();
-
-      const markerConfig = {
-        0: {"c": 200, "i": 1000},
-        1: {"c": 200, "i": 1000},
-        2: {"c": 200, "i": 500},
-        3: {"c": 400, "i": 300},
-        4: {"c": 500, "i": 200},
-        5: {"c": 900, "i": 100},
-        6: {"c": 2000, "i": 50},
-        7: {"c": 2000, "i": 30},
-        8: {"c": 2000, "i": 30},
-        9: {"c": 2000, "i": 5},
-        10: {"c": 2000, "i": 5},
-        11: {"c": 2000, "i": 2},
-        12: {"c": 2000, "i": 2},
-        13: {"c": 2000, "i": 1},
-        14: {"c": 2000, "i": 1},
-        15: {"c": 2000, "i": 1},
-        16: {"c": 2000, "i": 1},
-        17: {"c": 2000, "i": 1},
-        18: {"c": 2000, "i": 1},
-        19: {"c": 2000, "i": 1},
-        20: {"c": 2000, "i": 1},
-        21: {"c": 2000, "i": 1},
-        23: {"c": 2000, "i": 1},
-      }
       
       // 軌跡
       let dt = new Date();
@@ -306,8 +321,7 @@ export default {
       this.viewSatMarker();
 
       // 軌道と時刻マーカー
-      const pointCount = markerConfig[this.gmap.getZoom()].c;
-      const interval = markerConfig[this.gmap.getZoom()].i;
+      const interval = this.markerConfig[this.gmap.getZoom()];
       for(let ii = 0; ii < 10000; ii++){
         this.satellite.setDate(dt);
         this.satellite.refresh()
@@ -322,9 +336,9 @@ export default {
             fontSize: '8px'
           };
         }
-
         satPoints.push(this.satellite.position);
 
+        // 時刻マーカーの出力
         if(ii % interval == 0) {
           var markerOptions = {
             map: this.gmap,
@@ -353,6 +367,7 @@ export default {
       this.orbitLine.setMap(this.gmap);
     },
 
+    // TLEの解析
     analyzeTle(tle) {
       const tleArr = tle.split(/\r\n|\n/);
 
@@ -398,10 +413,10 @@ export default {
       return orbitMap
     },
 
-    onRealtimeClick(){
-      
-    },
+    // onRealtimeClick(){
+    // },
 
+    // メニュー・衛星名クリック
     selSatClick(satName) {
       this.target = satName;
 
@@ -412,10 +427,12 @@ export default {
       this.viewSat();
     },
 
+    // メニュー開閉
     onSelSatClick() {
       document.getElementById("selSatArea").classList.toggle("show");
     },
 
+    // メニュー・衛星名のサジェスト
     onSatSearchKeyup() {
       const input = document.getElementById("satSearchText");
       const filter = input.value.toUpperCase();
@@ -429,10 +446,6 @@ export default {
           a[i].style.display = "none";
         }
       }
-    },
-
-    onGmapZoomChanged(gmap) {
-      this.refreshOrbitTimer();
     },
   },
 
